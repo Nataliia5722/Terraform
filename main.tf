3614d5ae235ef44ca13790da08583ec6040fdc84
Nataliia5722 <71754022+Nataliia5722@users.noreply.github.com>
2021-02-05 20:47:58 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,147 @@
+provider "aws" {
+  region  = "us-east-1"   
+  access_key= "AKIAIKYR7UOP3F5T3UIA"
+  secret_key= "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"
+}
+
+# Variable to create n instances
+variable "instance_count" {
+  default = "2"
+}
+
+resource "aws_vpc" "prod-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
+}
+
+# Creating of Internet Gateway
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
+}
+# Creating of Custom Route Table
+
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Prod"
+  }
+}
+
+# Creating a Subnet 
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "prod-subnet"
+  }
+}
+
+# Associating subnet with Route Table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+# Creating Security Group to allow port HTTP port 80, HTTPS 443 and SSH 22
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow Web inbound traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  
+ ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+# Creating a network interface with an ip in the subnet that was created earlier
+
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+
+}
+# Assigning an elastic IP to the network interface created earlier
+
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.gw]
+}
+
+output "server_public_ip" {
+  value = aws_eip.one.public_ip
+}
+
+# Creating Windows Server 2019 2 instances 
+
+resource "aws_instance" "web-server-instance" {
+  ami               = "ami-0f5761c546ea1265a"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  #key_name          = "main-key"
+  count = var.instance_count
+
+  tags = {
+    Name = "web-server"
+  }
+}
+
+
+
+# output "server_private_ip" {
+#   value = aws_instance.web-server-instance.private_ip
+
+# }
+
+# output "server_id" {
+#   value = aws_instance.web-server-instance[count]
+# }
